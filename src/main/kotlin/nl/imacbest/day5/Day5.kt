55dfa9cfee297bb7f8e6ad2f7c375a3fd7dd1174
Thomas <thomas.erin999@gmail.com>
2021-12-06 20:58:57 +0100

    Day 5 challenge rewrite

--- src/main/kotlin/nl/imacbest/day5/Day5.kt
+++ src/main/kotlin/nl/imacbest/day5/Day5.kt
@@ -1,118 +1,56 @@
 package nl.imacbest.day5
 
-import java.lang.Integer.parseInt
+import kotlin.math.sign
 
 class Day5 {
 
     fun calculateIntersectingLinePoints(input: List<String>, onlyHorizontal: Boolean = true): Int {
-        val lines = parseLines(input)
-        val gridMaxX = findMaxX(lines) + 1
-        val gridMaxY = findMaxY(lines) + 1
-        val grid = Array<IntArray>(gridMaxX) { IntArray(gridMaxY) }
-        fillGridWithLines(grid, lines, onlyHorizontal)
-        return countIntersections(grid)
-    }
-
-    private fun fillGridWithLines(grid: Array<IntArray>, lines: List<Line>, onlyHorizontal: Boolean = true) {
-        lines
-            .filter { it ->
-                if (onlyHorizontal) {
-                    it.x1 == it.x2 || it.y1 == it.y2
-                } else {
-                    true
+        val ventMap = mutableMapOf<Point, Int>()
+        parseLines(input)
+            .flatMap { line -> createLines(line, onlyHorizontal) }
+            .forEach { point ->
+                ventMap.merge(point, 1) { a, b ->
+                    a + b
                 }
             }
-            .forEach { line ->
-                var xLow = line.getXStart() - 1
-                val xHigh = line.getXEnd()
-                var yLow = line.getYStart() - 1
-                val yHigh = line.getYEnd()
-                while (xLow < xHigh || yLow < yHigh) {
-                    if (xLow < xHigh) {
-                        xLow++
-                    }
-                    if (yLow < yHigh) {
-                        yLow++
-                    }
-                    grid[xLow][yLow]++
-                }
-
-            }
-    }
-
-    private fun countIntersections(grid: Array<IntArray>): Int {
-        var counter = 0;
-        for (x in grid.indices) {
-            for (y in grid.indices) {
-                if (grid[x][y] >= 2) {
-                    counter++
-                }
-            }
-        }
-        return counter
-    }
-
-    private fun findMaxX(lines: List<Line>) = lines.maxOf {
-        if (it.x1 > it.x2) {
-            it.x1
-        } else {
-            it.x2
-        }
-    }
-
-    private fun findMaxY(lines: List<Line>) = lines.maxOf {
-        if (it.y1 > it.y2) {
-            it.y1
-        } else {
-            it.y2
-        }
+        return ventMap.count { it.value > 1 };
     }
 
     private fun parseLines(input: List<String>): List<Line> =
         input.map { it ->
-            val spliter = it.split("->")
-            val startCords = spliter[0].split(",")
-                .map { cords ->
-                    parseInt(cords.trim())
-                }
-            val endCords = spliter[1].split(",")
-                .map { cords ->
-                    parseInt(cords.trim())
-                }
+            val (startX, startY) = it.substringBefore(" -> ").split(',').map(String::toInt)
+            val (endX, endY) = it.substringAfter(" -> ").split(',').map(String::toInt)
             Line(
-                x1 = startCords[0],
-                y1 = startCords[1],
-                x2 = endCords[0],
-                y2 = endCords[1]
+                Point(startX, startY), Point(endX, endY)
             )
         }
-}
 
-data class Line(
-    val x1: Int,
-    val y1: Int,
-    val x2: Int,
-    val y2: Int
-) {
-    fun getXStart(): Int = getHighest(x1, x2)
-
-    fun getXEnd(): Int = getLowest(x1, x2)
-
-    fun getYStart(): Int = getHighest(y1, y2)
-
-    fun getYEnd(): Int = getLowest(y1, y2)
-
-    private fun getHighest(a: Int, b: Int) =
-        if (a >= b) {
-            b
-        } else {
-            a
+    private fun createLines(line: Line, onlyHorizontal: Boolean = true): List<Point> {
+        val pointList = mutableListOf<Point>()
+        val delta = Point(
+            x = line.p2.x - line.p1.x,
+            y = line.p2.y - line.p1.y
+        ).deltafy()
+        if (onlyHorizontal && (delta.x != 0 && delta.y != 0)) {
+            return listOf()
         }
 
-    private fun getLowest(a: Int, b: Int) =
-        if (a >= b) {
-            a
-        } else {
-            b
+        pointList.add(line.p1)
+        var cur = line.p1
+
+        while (cur != line.p2) {
+            cur = Point(
+                x = cur.x + delta.x,
+                y = cur.y + delta.y
+            )
+            pointList.add(cur)
         }
+        return pointList
+    }
+
+    private fun Point.deltafy(): Point = Point(x = this.x.sign, y = this.y.sign)
 }
+
+data class Line(val p1: Point, val p2: Point)
+
+data class Point(val x: Int, val y: Int)
--- src/test/kotlin/nl/imacbest/day5/Day5Test.kt
+++ src/test/kotlin/nl/imacbest/day5/Day5Test.kt
@@ -3,35 +3,50 @@ package nl.imacbest.day5
 import nl.imacbest.utils.readFileToStringList
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
+@ExperimentalTime
 internal class Day5Test {
     private val day5 = Day5()
 
     @Test
     fun `Should calculate number of intersecting lines that cross twice from sample data`() {
         val input = readFileToStringList("day5/sample_input.txt")
-        val result = day5.calculateIntersectingLinePoints(input)
+        val (result, elapsed) = measureTimedValue {
+            day5.calculateIntersectingLinePoints(input)
+        }
+        println("Took: $elapsed")
         assertEquals(5, result)
     }
 
     @Test
     fun `Should calculate number of intersecting lines that cross twice from puzzle data`() {
         val input = readFileToStringList("day5/puzzle1.txt")
-        val result = day5.calculateIntersectingLinePoints(input)
+        val (result, elapsed) = measureTimedValue {
+            day5.calculateIntersectingLinePoints(input)
+        }
+        println("Took: $elapsed")
         assertEquals(8060, result)
     }
 
     @Test
     fun `Should calculate number of intersecting lines that cross twice with diagonal from sample data`() {
         val input = readFileToStringList("day5/sample_input.txt")
-        val result = day5.calculateIntersectingLinePoints(input, false)
+        val (result, elapsed) = measureTimedValue {
+            day5.calculateIntersectingLinePoints(input, false)
+        }
+        println("Took: $elapsed")
         assertEquals(12, result)
     }
 
     @Test
     fun `Should calculate number of intersecting lines that cross twice  with diagonal from puzzle data`() {
         val input = readFileToStringList("day5/puzzle1.txt")
-        val result = day5.calculateIntersectingLinePoints(input, false)
-        assertEquals(-1, result)
+        val (result, elapsed) = measureTimedValue {
+            day5.calculateIntersectingLinePoints(input, false)
+        }
+        println("Took: $elapsed")
+        assertEquals(21577, result)
     }
 }
